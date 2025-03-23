4be55632476465e3c3f188dcd0308dc5ca86956b
Josef Dosoudil <jeptun@gmail.com>
2025-03-23 18:15:27 +0100

    feat: initial frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import {Button} from './components/ui/button'
+
+
+function App() {
+
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Vite + React</h1>
+            <div className="flex flex-col items-center justify-center min-h-svh">
+                <Button>Click me</Button>
+            </div>
+        </>
+    )
+}
+
+export default App
